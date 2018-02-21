dc57c943e3f4ffddcb2f2466f6307e67d7532d17
Tristan Leveau <tristan.leveau@hei.yncrea.fr>
2018-02-21 07:26:44 +0100

    2.1

--- src/main/webapp/js/javascriptNFF.js
+++ src/main/webapp/js/javascriptNFF.js
@@ -1,3 +1,20 @@
 function AccesProtege() {
     self.location.href=prompt("Mot de passe :","");
 }
+
+function open_infos()
+{
+    width = 300;
+    height = 200;
+    if(window.innerWidth)
+    {
+        var left = (window.innerWidth-width)/2;
+        var top = (window.innerHeight-height)/2;
+    }
+    else
+    {
+        var left = (document.body.clientWidth-width)/2;
+        var top = (document.body.clientHeight-height)/2;
+    }
+    window.open('livraisondetail','Détails de la livraison','menubar=yes, scrollbars=yes, top='+top+', left='+left+', width='+width+', height='+height+'');
+}
